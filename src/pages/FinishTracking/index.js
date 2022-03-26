8bbc579c81438dabad78ef9fbbf3e5dd94548ed0
lisdrl <lisandre.darioli@gmail.com>
2022-03-26 18:35:09 -0300

    Insertion of StopTrackingButton in FinishTracking page

--- src/pages/FinishTracking/index.js
+++ src/pages/FinishTracking/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import StopTrackingButton from "../../components/StopTrackingButton";
 
 import { Container } from './styles';
 
 function FinishTracking() {
-  return <Container>botoes finalizar tracking</Container>;
+  return (
+    <Container>
+      <StopTrackingButton buttonText={"Concluir"} newStatus={"DELIVERED"} />
+      <StopTrackingButton buttonText={"Cancelar"} newStatus={"CANCELED"} />
+    </Container>
+  );
 }
 
 export default FinishTracking;
